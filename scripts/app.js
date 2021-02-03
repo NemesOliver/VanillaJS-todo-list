8fd6cc7aa82ecff6be024e14e8598b1e813aab48
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-03 16:56:52 +0000

    print user input

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,27 @@
   //event handlers
   addBtn.addEventListener("click", () => {
     const userInput = inputField.value;
-    //console.log(userInput);
+    console.log(userInput);
+    inputField.value = "";
+
+    //Create todo div with user input
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+    todo.innerHTML = `<p>${userInput}</p>
+     <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20 0C8.955 0 0 8.955 0 20C0 31.045 8.955 40 20 40C31.045 40 40 31.045 40 20C40 8.955 31.045 0 20 0ZM27.6617 29.9233L20.01 22.34L12.4183 30L10.0767 27.6583L17.655 20.005L10 12.4183L12.3417 10.0767L19.9883 17.6483L27.5633 10L29.9233 12.3383L22.3467 19.9833L30 27.5633L27.6617 29.9233Z"
+        fill="black"
+      />
+    </svg>`;
+    // append to this div
+    const container = document.querySelector(".todo-container");
+    container.appendChild(todo);
   });
 })();
